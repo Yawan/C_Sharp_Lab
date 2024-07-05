9b6fe8ffdf5c9240db722e6c04b94529a7b5d8fd
Yaw_An <b97502027@gmail.com>
2024-07-05 16:47:35 +0800

    use Bootstrapper to Display ShellView, and then remove MainWindow.xaml

--- RetailManager/RetailDesktopUI/Views/ShellView.xaml.cs
+++ RetailManager/RetailDesktopUI/Views/ShellView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RetailDesktopUI
+namespace RetailDesktopUI.Views
 {
     /// <summary>
-    /// MainWindow.xaml 的互動邏輯
+    /// ShellView.xaml 的互動邏輯
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ShellView : Window
     {
-        public MainWindow()
+        public ShellView()
         {
             InitializeComponent();
         }
