4efbc02240be532169f602575b8de282cf58d2b0
Yaw_An <b97502027@gmail.com>
2024-07-11 22:13:34 +0800

    add login view, using User Control template

--- RetailManager/RetailDesktopUI/ViewModels/ShellViewModel.cs
+++ RetailManager/RetailDesktopUI/ViewModels/ShellViewModel.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RetailDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loginVM;
+
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loginVM = loginVM;
+            ActivateItemAsync(_loginVM);
+
+            //var context = SynchronizationContext.Current;
+            //context.Send(async _ =>
+            //{
+            //    await ActivateItemAsync(_loginVM);
+            //}, null);
+        }
     }
 }
