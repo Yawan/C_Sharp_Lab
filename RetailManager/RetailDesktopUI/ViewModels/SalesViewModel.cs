7fadb3015522587450a44fcd858c918dad67cadc
Yaw_An <b97502027@gmail.com>
2024-08-20 13:11:06 +0800

    add Sales Page

---
+++ RetailManager/RetailDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,123 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _cart;
+        private BindingList<string> _products;
+        private int _itemQuantity;
+
+        public string SubTotal
+        {
+            get
+            {
+                // TODO Replace with calculation
+                return "$0.00";
+            }
+        }
+
+        public string Tax
+        {
+            get
+            {
+                // TODO Replace with calculation
+                return "$0.00";
+            }
+        }
+
+        public string Total
+        {
+            get
+            {
+                // TODO Replace with calculation
+                return "$0.00";
+            }
+        }
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        public int ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+
+                // TODO Make sure something is selected
+                // TODO Make sure there is an item quantity
+
+                return output;
+            }
+        }
+
+        public void AddToCart()
+        {
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                // TODO Make sure something is selected
+
+                return output;
+            }
+        }
+
+        public void RemoveFromCart()
+        {
+        }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+                // TODO Make sure there is something in the cart
+
+                return output;
+            }
+        }
+
+        public void CheckOut()
+        {
+        }
+    }
+}
