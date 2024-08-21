59432e939d84989385e47b1b546c51a08d124c0e
Yaw_An <b97502027@gmail.com>
2024-08-21 13:20:44 +0800

    handle Login Event

--- RetailManager/RetailDesktopUI/ViewModels/ShellViewModel.cs
+++ RetailManager/RetailDesktopUI/ViewModels/ShellViewModel.cs
@@ -1,21 +1,29 @@
 ﻿using Caliburn.Micro;
+using RetailDesktopUI.EventModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 //using System.Threading;
 
 namespace RetailDesktopUI.ViewModels
 {
-    public class ShellViewModel : Conductor<object>
+    public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
         private LoginViewModel _loginVM;
+        private SalesViewModel _salesVM;
+        private IEventAggregator _events;
 
-        public ShellViewModel(LoginViewModel loginVM)
+        public ShellViewModel(IEventAggregator events, LoginViewModel loginVM, SalesViewModel salesVM)
         {
+            _events = events;
             _loginVM = loginVM;
+            _salesVM = salesVM;
+
+            _events.SubscribeOnPublishedThread(this);
             ActivateItemAsync(_loginVM);
 
             //var context = SynchronizationContext.Current;
@@ -23,6 +31,11 @@ namespace RetailDesktopUI.ViewModels
             //{
             //    await ActivateItemAsync(_loginVM);
             //}, null);
+        }
+
+        async Task IHandle<LogOnEvent>.HandleAsync(LogOnEvent message, CancellationToken cancellationToken)
+        {
+            await ActivateItemAsync(_salesVM);
         }
     }
 }
