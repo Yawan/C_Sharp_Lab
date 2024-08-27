6123636e8bba9c4e8d55e1e8d4eb0ca5228e6b4c
Yaw_An <b97502027@gmail.com>
2024-08-27 16:02:56 +0800

    Add api controller with Product

---
+++ RetailManager/DataManagerLibrary/DataAccess/ProductData.cs
@@ -0,0 +1,22 @@
+﻿using DataManager.Library.Internal.DataAccess;
+using DataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManager.Library.DataAccess
+{
+    public class ProductData
+    {
+        public List<ProductModel> GetProducts()
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var output = sql.LoadData<ProductModel, dynamic>("dbo.spProductGetAll", new { }, "RetailData");
+
+            return output;
+        }
+    }
+}
