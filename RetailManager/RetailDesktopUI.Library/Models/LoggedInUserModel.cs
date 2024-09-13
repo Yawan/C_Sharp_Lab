f43a9cbdd36e0de253d4891401dac3ad12686d6e
Joe <yawan_shieh@dsa-auto.com.tw>
2024-09-13 15:01:14 +0800

    add LogOut in menu and Clear info after LogOut

--- RetailManager/RetailDesktopUI.Library/Models/LoggedInUserModel.cs
+++ RetailManager/RetailDesktopUI.Library/Models/LoggedInUserModel.cs
@@ -14,5 +14,15 @@ namespace RetailDesktopUI.Library.Models
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
         public DateTime CreatedDate { get; set; }
+
+        public void LogOffUser()
+        {
+            Token = "";
+            Id = "";
+            FirstName = "";
+            LastName = "";
+            EmailAddress = "";
+            CreatedDate = DateTime.Now;
+        }
     }
 }
--- RetailManager/RetailDesktopUI/ViewModels/ShellViewModel.cs
+++ RetailManager/RetailDesktopUI/ViewModels/ShellViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Caliburn.Micro;
 using RetailDesktopUI.EventModels;
+using RetailDesktopUI.Library.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,19 +17,39 @@ namespace RetailDesktopUI.ViewModels
         private SalesViewModel _salesVM;
         private IEventAggregator _events;
 
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
+        private ILoggedInUserModel _user;
+
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM, ILoggedInUserModel user)
         {
             _events = events;
             _salesVM = salesVM;
+            _user = user;
 
             _events.SubscribeOnPublishedThread(this);
 
             ActivateItemAsync(IoC.Get<LoginViewModel>());
         }
 
+        public bool IsLoggedIn
+        {
+            get
+            {
+                // if we are logged in, token is shouldn't be null or empty string
+                return string.IsNullOrWhiteSpace(_user.Token) == false;
+            }
+        }
+
+        public void LogOut()
+        {
+            _user.LogOffUser();
+            ActivateItemAsync(IoC.Get<LoginViewModel>());
+            NotifyOfPropertyChange(() => IsLoggedIn);
+        }
+
         async Task IHandle<LogOnEvent>.HandleAsync(LogOnEvent message, CancellationToken cancellationToken)
         {
             await ActivateItemAsync(_salesVM);
+            NotifyOfPropertyChange(() => IsLoggedIn);
         }
 
         public async Task ExitApplication()
